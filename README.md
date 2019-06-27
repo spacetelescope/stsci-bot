695559841f1c464646edfb937d16d09a8d1f877c
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-06-27 15:07:37 -0400

    Initial commit based on astropy-bot

---
+++ README.md
@@ -0,0 +1,8 @@
+# stsci-bot
+
+This is a bot for STScI repository, primarily used by DATB and SCSB.
+It is similar to [astropy-bot](https://github.com/astropy/astropy-bot) and
+uses [baldrick](https://github.com/OpenAstronomy/baldrick).
+
+For the older iteration of this bot, please see
+[stsci-bot-old](https://github.com/spacetelescope/stsci-bot-old).
---
+++ run.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+from baldrick import create_app
+
+# Configure the app
+app = create_app('stsci-bot')
+
+# Load plugins from baldrick
+import baldrick.plugins.github_milestones  # noqa
+
+# Load STScI-specific plugins
+import stsci_bot.changelog_checker  # noqa
+
+# Bind to PORT if defined, otherwise default to 5000.
+port = int(os.environ.get('PORT', 5000))
+
+if '--skip-run' not in sys.argv:
+    app.run(host='0.0.0.0', port=port, debug=False)
